b0765e774ef19d0b2996c9e07b156689f0e54a6d
abhishekgtruxt <abhishekg@truxt.ai>
2024-09-06 19:37:53 +0530

    Home Page Created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import AuthCard from './Components/Cards/AuthCard/AuthCard';
-import UnAuthCard from './Components/Cards/UnAuthCard/UnAuthCard';
-import { useState } from 'react';
+import "./App.css";
+import AuthCard from "./Components/Cards/AuthCard/AuthCard";
+import UnAuthCard from "./Components/Cards/UnAuthCard/UnAuthCard";
+import { useState } from "react";
+import SelectCard from "./Components/SelectCard/SelectCard";
 
 function App() {
+  const [DisplayCard, setDisplayCard] = useState(false);
 
   return (
-    <div className='screen'>
-      
-        {/* <UnAuthCard/> */}
-        <AuthCard />
-      
+    <div className="screen">
+      {DisplayCard === "unauth" ? <UnAuthCard />:null}
+      {DisplayCard === "auth" ? <AuthCard /> : <SelectCard />}
     </div>
   );
 }
---
+++ src/Components/SelectCard/SelectCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./SelectCard.css";
+
+function SelectCard() {
+  return (
+    <div className='SelectCard'>
+      <h1>Select the appropriate card</h1>
+      <button>Auth Card</button>
+      <button>UnAuth Card</button>
+    </div>
+  )
+}
+
+export default SelectCard;
